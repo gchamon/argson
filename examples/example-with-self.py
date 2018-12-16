c1c12f132c13019bd2f3fa2104d7b7d3e019ca82
Gabriel Chamon Araujo <gchamon@live.com>
2018-12-16 21:21:41 -0200

    Update example-with-self.py

--- examples/example-with-self.py
+++ examples/example-with-self.py
@@ -5,7 +5,7 @@ PATH = os.path.dirname(os.path.realpath(__file__))
 
 def main():
     argson.parse_file_and_arguments('config/example.json', self_file='config/self-example.json', working_dir=PATH)
-    print("err... you probably want to run this file with the flag '--help' to see the effects of self_file flag")
+    print("err... you probably want to run this file with the flag '--help' to see the effects of self_file argument")
 
 
 if __name__ == "__main__":
